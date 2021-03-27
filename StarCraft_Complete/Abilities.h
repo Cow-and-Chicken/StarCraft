c0e6fa3e011887280279dfb8be60f41ac4e9e8b3
AngelovD <71969791+AngelovD@users.noreply.github.com>
2021-03-27 02:50:40 +0200

    Complete version without CMake
    Passed all of the test cases and debugged the old version

---
+++ StarCraft_Complete/Abilities.h
@@ -0,0 +1,15 @@
+#ifndef ABILITIES_H_
+#define ABILITIES_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "Defines.h"
+
+struct Abilities{
+    int (*attack)(Ship *attacker,List *targetFleet);
+    int (*getDMG)(Ship *ship,int dmg);
+    void (*specialAbility)(Ship *ship);
+    void (*printShip)(Ship *ship);
+};
+
+#endif /* ABILITIES_H_ */
